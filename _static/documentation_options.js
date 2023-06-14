776b79fc39906d693a5840746a9a67afb36083cb
tkoyama010 <tkoyama010@users.noreply.github.com>
2023-06-14 08:40:36 +0000

    deploy: pyvista/pyvistaqt@0ace604590dc14b5837c65e4b8962d5e9247ffc0

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0.dev1+g8c83bbd',
+    VERSION: '0.1.0.dev1+g0ace604',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
