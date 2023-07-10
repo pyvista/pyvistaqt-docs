abbaf9be6cb15b2f8983538603451d63c6c16425
larsoner <larsoner@users.noreply.github.com>
2023-07-10 15:39:24 +0000

    deploy: pyvista/pyvistaqt@8e78a65fabf9d80e3e4f636c536f65608d714c0a

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0.dev1+g2c0a138',
+    VERSION: '0.1.0.dev1+g8e78a65',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
